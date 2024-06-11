07ae13a52c70a1044fb74a9720a2bfaa317c9bb1
Lina Hsu <linahthsu@gmail.com>
2024-06-11 10:34:08 -0300

    feat: implements exercise_animal.py to practice multilevel heritage

---
+++ 5-Eletiva-Python/secao-2-Programação-Orientada-a-Objetos/dia-2-Tipagem-e-Pilares-da-POO/Herança e Composiçao/mixins.py
@@ -0,0 +1,3 @@
+# Uma forma de amenizar o problema da herança múltipla é o uso de mixins. No
+# caso, são classes que não têm estado, mas apenas métodos, que podem ser
+# utilizados por outras classes.
