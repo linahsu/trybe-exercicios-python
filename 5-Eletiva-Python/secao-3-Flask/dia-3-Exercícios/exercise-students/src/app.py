987a77075a8023f503c90460718dddc6c5cb1df3
Lina Hsu <linahthsu@gmail.com>
2024-06-21 10:54:43 -0300

    feat: implements endpoints to list all students, add and edit, and the templates for it

--- 5-Eletiva-Python/secao-3-Flask/dia-3-Exercícios/exercise-students/src/app.py
+++ 5-Eletiva-Python/secao-3-Flask/dia-3-Exercícios/exercise-students/src/app.py
@@ -1,8 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from os import environ
 from waitress import serve
 
 app = Flask(__name__)
+
+students = [
+  {"name": "Ana", "register": 1234},
+  {"name": "Bruno", "register": 5678},
+  {"name": "Carlos", "register": 2345},
+  {"name": "Daniela", "register": 6789},
+  {"name": "Eduardo", "register": 3456},
+]
+
+
+@app.route("/")
+@app.route("/alunos")
+def get_all_students():
+   return render_template('students.html', students=students)
+
+
+@app.route("/alunos/adicionar", methods=["GET", "POST"])
+def add_student():
+  if request.method == "POST":
+    name = request.form.get('name')
+    register = request.form.get('register')
+
+    students.append({
+        "name": name,
+        "register": int(register)
+    })
+
+    return redirect('/')
+  
+  return render_template('add_student.html')
+
+
+@app.route("/alunos/editar/<int:index>", methods=["GET", "POST"])
+def update_student(index):
+  if request.method == "POST":
+    students[index]["name"] = request.form.get('name')
+    students[index]["register"] = request.form.get('register')
+    return redirect('/')
+  
+  return render_template('update_student.html', student=students[index], aluno_index=index)
 
 
 def start_server(host: str = '0.0.0.0', port: int = 8000):
