b6474b7cf4e03ba5ddf9a25f17e6803f8e99dfae
Lina Hsu <linahthsu@gmail.com>
2024-06-24 14:49:15 -0300

    feat: implements tests for tasks page

--- 5-Eletiva-Python/secao-3-Flask/dia-5-API-e-Testes-em-Flask/exercise-project-manager/tests/test_task_page.py
+++ 5-Eletiva-Python/secao-3-Flask/dia-5-API-e-Testes-em-Flask/exercise-project-manager/tests/test_task_page.py
@@ -13,3 +13,33 @@ def test_title_of_task(client, seed_project):
     task = db.get_collection("projects").find_one({ "idProject": 1 })
     response = client.get(f"/task/{task['_id']}")
     assert task["task"] in response.text
+
+def test_name_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert task["name"] in response.text
+
+def test_description_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert task["descriptionTask"] in response.text
+
+def test_status_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert task["status"] in response.text
+
+def test_conclusion_percentage_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert str(task["completionPercentage"]) in response.text
+
+def test_deadline_date_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert task["deadline"] in response.text
+
+def test_responsible_of_task(client, seed_project):
+  task = db.get_collection("projects").find_one({ "idProject": 1 })
+  response = client.get(f"/task/{task['_id']}")
+  assert task["responsible"] in response.text
