695f0283128ef21d81f88150fc1a5d746687bcc8
Lina Hsu <linahthsu@gmail.com>
2024-06-24 09:53:59 -0300

    feat: implements products page

---
+++ 5-Eletiva-Python/secao-3-Flask/dia-4-Server-Side-Rendering-em-Flask/exercise-book/src/controllers/product_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, request, redirect
+from models.product_model import Product
+
+product_controller = Blueprint("product_controller", __name__)
+
+products = []
+
+@product_controller.route("/", methods=["GET"])
+def product_index():
+  products_dict = [product.to_dict() for product in products]
+  return render_template("products.html", products=products_dict)
+
+
+@product_controller.route("/add", methods=["GET", "POST"])
+def add_new_product():
+  if request.method == "POST":
+    id = int(request.form.get("id"))
+    name = request.form.get("name")
+    price = float(request.form.get("price"))
+
+    products.append(Product(id, name, price))
+    
+    return redirect("/")
+  
+  return render_template("add_product.html")
