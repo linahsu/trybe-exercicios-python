6381a88f331cd0204fcf01ff3739248fbed451e8
Lina Hsu <linahthsu@gmail.com>
2024-06-24 14:42:58 -0300

    feat: implements tests for project_page

--- 5-Eletiva-Python/secao-3-Flask/dia-5-API-e-Testes-em-Flask/exercise-project-manager/tests/test_project_page.py
+++ 5-Eletiva-Python/secao-3-Flask/dia-5-API-e-Testes-em-Flask/exercise-project-manager/tests/test_project_page.py
@@ -22,3 +22,24 @@ def test_title_of_tasks(client, seed_project):
   task_2 = "<p>Task 2</p>"
   assert task_1 in response.text
   assert task_2 in response.text
+
+def test_status_of_tasks(client, seed_project):
+  response = client.get("/projects/1")
+  task_1_status = '<p>To Do</p>'
+  task_2_status = '<p>Doing</p>'
+  assert task_1_status in response.text
+  assert task_2_status in response.text
+
+def test_tasks_responsible(client, seed_project):
+  response = client.get("/projects/1")
+  task_1_responsible = '<p>Responsible 1</p>'
+  task_2_responsible = '<p>Responsible 2</p>'
+  assert task_1_responsible in response.text
+  assert task_2_responsible in response.text
+
+def test_tasks_description(client, seed_project):
+  response = client.get("/projects/1")
+  task_1_description = '<p>Descrição: Description Task 1</p>'
+  task_2_description = '<p>Descrição: Description Task 2</p>'
+  assert task_1_description in response.text
+  assert task_2_description in response.text
