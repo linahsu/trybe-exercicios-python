48f10a8be92bd902f7cff232baeebd6d3c298411
Lina Hsu <linahthsu@gmail.com>
2024-06-24 00:12:37 -0300

    feat: implements homepage

--- 5-Eletiva-Python/secao-3-Flask/dia-4-Server-Side-Rendering-em-Flask/exercise-book/src/app.py
+++ 5-Eletiva-Python/secao-3-Flask/dia-4-Server-Side-Rendering-em-Flask/exercise-book/src/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from os import environ
 from waitress import serve
+from controllers.home_controller import home_controller
 
 app = Flask(__name__)
 
 app.template_folder = "views/templates"
 
+app.register_blueprint(home_controller, url_prefix="/")
+
 def start_server(host = "0.0.0.0", port = 8000):
   if environ.get("FLASK_ENV") == "dev":
-    return app.run(debub=True, host=host, port=port)
+    return app.run(debug=True, host=host, port=port)
   else:
     serve(app, host=host, port=port)
 
