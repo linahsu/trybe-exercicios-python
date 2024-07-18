a06e6ec2b45c9c11209befc1dfcab89544148294
Lina Hsu <linahthsu@gmail.com>
2024-07-18 15:14:37 -0300

    feat: creates template and rout about

--- 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/event-manager/events/urls.py
+++ 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/event-manager/events/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from events.views import index
-
+from events.views import index, about
 
 urlpatterns = [
-    path("", index, name="home-page")
+    path("", index, name="home-page"),
     #   path("/rota-comentada", função-que-será-executada, name="nome-que-identifica-a-rota") # noqa E501
+    path("about", about, name="about-page")
 ]
