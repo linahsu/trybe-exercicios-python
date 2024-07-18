f0aae31a11f2c95f1fb917610001e15d5858808a
Lina Hsu <linahthsu@gmail.com>
2024-07-18 18:15:55 -0300

    feat: implements template details

--- 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/event-manager/events/views.py
+++ 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/event-manager/events/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from events.models import Event
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -12,3 +13,8 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def event_details(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, 'details.html', {'event': event})
