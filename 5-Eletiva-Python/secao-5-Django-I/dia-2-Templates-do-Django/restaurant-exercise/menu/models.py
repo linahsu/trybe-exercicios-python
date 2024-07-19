dcba3f260a61e5be12689d14c80d6992cdf98435
Lina Hsu <linahthsu@gmail.com>
2024-07-19 12:42:31 -0300

    feat: implements class model Recipe

--- 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/restaurant-exercise/menu/models.py
+++ 5-Eletiva-Python/secao-5-Django-I/dia-2-Templates-do-Django/restaurant-exercise/menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    # ingredients: dicionário contendo o ingrediente e quantidade
+    ingredients = models.JSONField(default=dict)
+    instructions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="media/img", blank=True)
+
+    def __str__(self):
+        return self.name
