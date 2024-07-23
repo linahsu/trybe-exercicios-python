431e9304536b4b8fc86cd527af37e4bd0cfa535f
Lina Hsu <linahthsu@gmail.com>
2024-07-23 16:32:54 -0300

    feat: implements a modelForm - CreateMusicModelForm

--- 5-Eletiva-Python/secao-5-Django-I/dia-4-Formulários-e-Relacionamento-de-modelos/playlist/playlist/forms.py
+++ 5-Eletiva-Python/secao-5-Django-I/dia-4-Formulários-e-Relacionamento-de-modelos/playlist/playlist/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from playlist.validators import validate_music_length, validate_name_is_char
-from playlist.models import Music
+from playlist.models import Music, Singer
 
 
 class CreateMusicForm(forms.Form):
@@ -30,17 +30,39 @@ class CreateSingerForm(forms.Form):
     name = forms.CharField(max_length=50, validators=[validate_name_is_char])
 
 
+# class CreateMusicModelForm(forms.ModelForm):
+#     class Meta:
+#         model = Music
+#         fields = "__all__"
+#         labels = {
+#             "name": "Nome da música",
+#             "recorded_at": "Data de gravação",
+#             "length_in_seconds": "Duração em segundos",
+#         }
+#         widgets = {
+#             "recorded_at": forms.DateInput(
+#                 attrs={"type": "date", "value": "2023-07-06"}
+#             )
+#         }
+
+
 class CreateMusicModelForm(forms.ModelForm):
     class Meta:
         model = Music
         fields = "__all__"
-        labels = {
-            "name": "Nome da música",
-            "recorded_at": "Data de gravação",
-            "length_in_seconds": "Duração em segundos",
-        }
-        widgets = {
-            "recorded_at": forms.DateInput(
-                attrs={"type": "date", "value": "2023-07-06"}
-            )
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].label = "Nome da música"
+        self.fields["recorded_at"].label = "Data de gravação"
+        self.fields["recorded_at"].widget = forms.DateInput(
+                attrs={"type": "date"})
+        self.fields["recorded_at"].initial = "2023-07-06"
+        self.fields["length_in_seconds"].label = "Duração em segundos"
+        self.fields["singer"].label = "Artista"
+        self.fields["singer"].widget = forms.Select(
+            choices=[
+                (singer.id, singer.name)
+                for singer in Singer.objects.filter(name__contains="a")
+            ]
+        )
