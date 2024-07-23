5e6da22f08312ff55f6c48ed5cefe522feb142d6
Lina Hsu <linahthsu@gmail.com>
2024-07-23 17:30:57 -0300

    feat: implements functions in views

--- 5-Eletiva-Python/secao-5-Django-I/dia-4-Formulários-e-Relacionamento-de-modelos/marketplace-exercise/products/views.py
+++ 5-Eletiva-Python/secao-5-Django-I/dia-4-Formulários-e-Relacionamento-de-modelos/marketplace-exercise/products/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from products.forms import CreateProductForm, CreateSellerForm, CreateBuyerForm, CreateOrderForm # noqa E501
+from products.models import Product, Seller, Buyer, Order
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def product(request):
+    form = CreateProductForm()
+
+    if request.method == "POST":
+        form = CreateProductForm(request.POST)
+
+        if form.is_valid():
+            Product.objects.create(**form.cleaned_data)
+
+    context = {"form": form}
+    return render(request, 'product.html', context)
+
+
+def seller(request):
+    form = CreateSellerForm()
+
+    if request.method == "POST":
+        form = CreateSellerForm(request.POST)
+
+        if form.is_valid():
+            Seller.objects.create(**form.cleaned_data)
+
+    context = {"form": form}
+    return render(request, 'seller.html', context)
+
+
+def buyer(request):
+    form = CreateBuyerForm()
+
+    if request.method == "POST":
+        form = CreateBuyerForm(request.POST)
+
+        if form.is_valid():
+            Buyer.objects.create(**form.cleaned_data)
+
+    context = {"form": form}
+    return render(request, 'buyer.html', context)
+
+
+def order(request):
+    form = CreateOrderForm()
+
+    if request.method == "POST":
+        form = CreateOrderForm(request.POST)
+
+        if form.is_valid():
+            Order.objects.create(**form.cleaned_data)
+
+    context = {"form": form}
+    return render(request, 'order.html', context)
