79d9b1709d7a52d0c0ef0aeb5d783f910a924f64
Lina Hsu <linahthsu@gmail.com>
2024-07-29 18:33:47 -0300

    feat: implements models, make migrations and register them in the super user panel

--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/admin.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from budget.models import Vendor, Marriage, Budget
 
-# Register your models here.
+
+admin.site.site_header = 'Weddings Manager Admin Panel'
+admin.site.register(Vendor)
+admin.site.register(Marriage)
+admin.site.register(Budget)
