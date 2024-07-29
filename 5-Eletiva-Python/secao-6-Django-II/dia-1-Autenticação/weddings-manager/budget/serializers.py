165fcb616e0dd46e561d2071711f043635928d84
Lina Hsu <linahthsu@gmail.com>
2024-07-29 20:13:18 -0300

    feat: implements serializer AdminVendorSerializer and refactor VendorViewSet to restrict with Basic Authentication

--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/serializers.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/serializers.py
@@ -32,3 +32,17 @@ class MarriageSerializer(serializers.ModelSerializer):
         budget_data['marriage'] = Marriage.objects.create(**validated_data)
         BudgetSerialiser().create(validated_data=budget_data)
         return budget_data['marriage']
+
+
+class AdminVendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = '__all__'
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and request.user.is_superuser:
+            return super().create(validated_data)
+        raise serializers.ValidationError(
+            "Você não tem permissão para criar fornecedores."
+        )
--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/views.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/views.py
@@ -1,12 +1,22 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
 from budget.models import Vendor, Marriage, Budget
-from budget.serializers import VendorSerializer, MarriageSerializer, BudgetSerialiser # noqa E501
+from budget.serializers import (
+    VendorSerializer,
+    MarriageSerializer,
+    BudgetSerialiser,
+    AdminVendorSerializer,
+)
 
 
 class VendorViewSet(viewsets.ModelViewSet):
     queryset = Vendor.objects.all()
-    serializer_class = VendorSerializer
+    serializer_class = AdminVendorSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("create", "destroy", "update"):
+            return AdminVendorSerializer
+        return VendorSerializer
 
 
 class MarriageViewSet(viewsets.ModelViewSet):
