0f1836703fb611f2cac9ac80fa148a3d7445cac9
Lina Hsu <linahthsu@gmail.com>
2024-07-29 20:33:40 -0300

    feat: implements permission class and refactor to use 'user' in Marriage and Budget models

--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/serializers.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/serializers.py
@@ -8,29 +8,58 @@ class VendorSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "price"]
 
 
-class BudgetSerialiser(serializers.ModelSerializer):
+class BudgetSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Budget
-        fields = ["id", "vendors", "marriage"]
+        fields = ["id", "vendors", "marriage", "user"]
 
 
 class NestedBudgetSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Budget
-        fields = ["id", "vendors"]
+        fields = ["id", "vendors", "user"]
 
 
 class MarriageSerializer(serializers.ModelSerializer):
     budget = NestedBudgetSerializer()
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
 
     class Meta:
         model = Marriage
-        fields = ["id", "codename", "date", "budget"]
+        fields = ["id", "codename", "date", "budget", "user"]
 
+    # def create(self, validated_data):
+    #     budget_data = validated_data.pop('budget')
+    #     budget_data['marriage'] = Marriage.objects.create(**validated_data)
+    #     BudgetSerializer().create(validated_data=budget_data)
+    #     return budget_data['marriage']
+
+    # Sem a inteligência do serializer precisamos unir os dados todos 'na mão'
     def create(self, validated_data):
+        # Recupera o user que fez a requisição
+        current_user = self.context['request'].user
+
+        '''
+        Recupera os dados de budget da requisição, acrescenta a eles
+        e aos dados de Marriage os dados do usuário
+        '''
         budget_data = validated_data.pop('budget')
+        budget_data['user'] = current_user
+        validated_data['user'] = current_user
+
+        # Cria marriage, cria budget e retorna Marriage, como a função pede
         budget_data['marriage'] = Marriage.objects.create(**validated_data)
-        BudgetSerialiser().create(validated_data=budget_data)
+        BudgetSerializer().create(validated_data=budget_data)
         return budget_data['marriage']
 
 
--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/views.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/weddings-manager/budget/views.py
@@ -1,17 +1,20 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
 from budget.models import Vendor, Marriage, Budget
+from rest_framework.authentication import BasicAuthentication
 from budget.serializers import (
     VendorSerializer,
     MarriageSerializer,
-    BudgetSerialiser,
+    BudgetSerializer,
     AdminVendorSerializer,
 )
+from budget.permissions import IsOwnerOrAdmin
 
 
 class VendorViewSet(viewsets.ModelViewSet):
     queryset = Vendor.objects.all()
     serializer_class = AdminVendorSerializer
+    authentication_classes = [BasicAuthentication]
 
     def get_serializer_class(self):
         if self.action in ("create", "destroy", "update"):
@@ -22,8 +25,32 @@ class VendorViewSet(viewsets.ModelViewSet):
 class MarriageViewSet(viewsets.ModelViewSet):
     queryset = Marriage.objects.all()
     serializer_class = MarriageSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        """
+        Quem for admin vê todos os casamentos.
+        Caso contrário, a pessoa só vê os próprios casamentos.
+        """
+        if self.request.user.is_superuser:
+            return Marriage.objects.all()
+        else:
+            return Marriage.objects.filter(user=self.request.user)
 
 
 class BugdetViewSet(viewsets.ModelViewSet):
     queryset = Budget.objects.all()
-    serializer_class = BudgetSerialiser
+    serializer_class = BudgetSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        """
+        Quem for admin vê todos os orçamentos.
+        Caso contrário, a pessoa só vê os próprios orçamentos.
+        """
+        if self.request.user.is_superuser:
+            return Budget.objects.all()
+        else:
+            return Budget.objects.filter(user=self.request.user)
