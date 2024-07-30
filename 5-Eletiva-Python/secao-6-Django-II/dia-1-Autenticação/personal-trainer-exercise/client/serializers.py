8930dce2fa821e94d11ef229193f9969ddf74ae6
Lina Hsu <linahthsu@gmail.com>
2024-07-30 14:14:05 -0300

    feat: implements and configure to use Autentification simpleJWT

--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/personal-trainer-exercise/client/serializers.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/personal-trainer-exercise/client/serializers.py
@@ -3,43 +3,48 @@ from client.models import Client, WorkoutPlan
 
 
 class WorkoutPlanSerializer(serializers.ModelSerializer):
-    personal = serializers.PrimaryKeyRelatedField(
+    personal_trainer = serializers.PrimaryKeyRelatedField(
         read_only=True, default=serializers.CurrentUserDefault()
     )
 
     class Meta:
         model = WorkoutPlan
-        fields = ["id", "client", "personal", "workout"]
+        fields = ["id", "client", "personal_trainer", "workout"]
 
 
 class NestedWorkoutPlanSerializer(serializers.ModelSerializer):
-    personal = serializers.PrimaryKeyRelatedField(
+    personal_trainer = serializers.PrimaryKeyRelatedField(
         read_only=True, default=serializers.CurrentUserDefault()
     )
 
     class Meta:
         model = WorkoutPlan
-        fields = ["id", "personal", "workout"]
+        fields = ["id", "personal_trainer", "workout"]
 
 
 class ClientSerializer(serializers.ModelSerializer):
     workout_plan = NestedWorkoutPlanSerializer()
-    personal = serializers.PrimaryKeyRelatedField(
+    personal_trainer = serializers.PrimaryKeyRelatedField(
         read_only=True, default=serializers.CurrentUserDefault()
     )
 
     class Meta:
         model = Client
         fields = [
-            "id", "name", "address", "created_at", "workout_plan", "personal"
+            "id",
+            "name",
+            "address",
+            "created_at",
+            "workout_plan",
+            "personal_trainer"
         ]
 
     def create(self, validated_data):
-        current_personal = self.context['request'].personal
+        current_personal = self.context['request'].user
 
         workout_data = validated_data.pop('workout_plan')
-        workout_data['personal'] = current_personal
-        validated_data['personal'] = current_personal
+        workout_data['personal_trainer'] = current_personal
+        validated_data['personal_trainer'] = current_personal
 
         workout_data['client'] = Client.objects.create(**validated_data)
         WorkoutPlanSerializer().create(validated_data=workout_data)
