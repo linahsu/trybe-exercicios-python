555443aa52c0f85f98843de70ddb968390bc2b33
Lina Hsu <linahthsu@gmail.com>
2024-07-30 12:28:19 -0300

    feat: configure to use mysql

--- 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/personal-trainer-exercise/personal_trainer/settings.py
+++ 5-Eletiva-Python/secao-6-Django-II/dia-1-Autenticação/personal-trainer-exercise/personal_trainer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'personal_trainer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'personal_trainer_database',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
